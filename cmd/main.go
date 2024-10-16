737d5f61ee07052d9ea718d10a9547966f1e5f26
OmarAguilar43 <166642943+OmarAguilar43@users.noreply.github.com>
2024-10-16 09:31:49 -0600

    funcion get

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Name  string
+	Email string
+}
 
 func main() {
-	fmt.Println("Works")
+	user := &User{
+		Name:  "Jose",
+		Email: "tilintilico@pilin.com",
+	}
+
+	users := []User{
+		{
+			Name:  "Omar",
+			Email: "omarooo@pilin.com",
+		},
+		{
+			Name:  "Miguel",
+			Email: "Miguelon@pilin.com",
+		},
+	}
+	router := gin.Default()
+	fmt.Println("Running app")
+	//Tomar archivos de la acarpeta templates
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+			"user":  user,
+			"users": users,
+		})
+	})
+	router.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
